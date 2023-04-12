3a0fff1dc190c63aa460fab07047b858895a1108
OisinWrites <oisin.banville@gmail.com>
2023-04-12 14:17:28 +0000

    Remove fixtures as not applicable, rename model to product, add content to views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    categories = Category.objects.filter(parent_category__isnull=True)
+    # filter top-level categories
+    return render(request, 'product_list.html', {'products': products,
+                                                 'categories': categories})
+
+
+def filter_products(request, category_id):
+    category = Category.objects.get(id=category_id)
+    products = Product.objects.filter(category=category)
+    subcategories = category.category_set.all()
+    # get subcategories
+    return render(request, 'product_list.html', {'products': products,
+                  'categories': subcategories, 'selected_category': category})
