b365062e6f0c0604d2a7c22903111e0c42c6463d
OisinWrites <oisin.banville@gmail.com>
2023-04-17 13:49:32 +0000

    Create view function to remove items

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -35,3 +35,53 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Add or remove a quantity of the specified product
+    to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    variant = None
+    if 'product_variant' in request.POST:
+        variant = request.POST['product_variant']
+    bag = request.session.get('bag', {})
+
+    if variant:
+        if quantity > 0:
+            bag[item_id]['items_by_variant'][variant] = quantity
+        else:
+            del bag[item_id]['items_by_variant'][variant]
+            if not bag[item_id]['items_by_variant']:
+                bag.pop[item_id]
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse("view_bag"))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove the specified product, or variant thereof,
+    from the shopping bag """
+
+    try:
+        variant = None
+        if 'product_variant' in request.POST:
+            variant = request.POST['product_variant']
+        bag = request.session.get('bag', {})
+
+        if variant:
+            del bag[item_id]['items_by_variant'][variant]
+            if not bag[item_id]['items_by_variant']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
