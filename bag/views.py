42308a5f1c32102d73282f78fed27c8d26df0d8c
OisinWrites <oisin.banville@gmail.com>
2023-04-17 12:05:09 +0000

    add logic for bag to update games showing variants and quantity

--- bag/views.py
+++ bag/views.py
@@ -14,12 +14,24 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    variant = None
+    if 'product_variant' in request.POST:
+        variant = request.POST['product_variant']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if variant:
+        if item_id in list(bag.keys()):
+            if variant in bag[item_id]['items_by_variant'].keys():
+                bag[item_id]['items_by_variant'][variant] += quantity
+            else:
+                bag[item_id]['items_by_variant'][variant] = quantity
+        else:
+            bag[item_id] = {'items_by_variant': {variant: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
