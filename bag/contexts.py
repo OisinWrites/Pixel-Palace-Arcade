8175c092ddd3d652052abb35b279c1082d69ce8b
OisinWrites <oisin.banville@gmail.com>
2023-06-07 16:15:13 +0000

    Explain code with docstring for bag app

--- bag/contexts.py
+++ bag/contexts.py
@@ -2,30 +2,49 @@ from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
+"""
+This code is a function that calculates
+the contents of the shopping bag.
+It iterates over the items in the bag, calculates the total price,
+quantity, and delivery cost based on the items and their prices.
+It also determines if the total price qualifies for free delivery
+based on the predefined threshold in settings.
+The function returns a dictionary containing all
+the calculated values for rendering in the templates.
+"""
 
 
 def bag_contents(request):
+    # Initialize variables
+    bag_items = []  # List to store items in the bag
+    total = 0  # Total price of all items in the bag
+    product_count = 0  # Total quantity of all products in the bag
+    bag = request.session.get('bag', {})  # Retrieve bag data from the session
 
-    bag_items = []
-    total = 0
-    product_count = 0
-    bag = request.session.get('bag', {})
-
+    # Iterate over items in the bag
     for item_id, item_data in bag.items():
+        # Check if item_data is an integer (item without variants)
         if isinstance(item_data, int):
+            # Retrieve the product using item_id
             product = get_object_or_404(Product, pk=item_id)
+            # Calculate the price and quantity for the item
             total += item_data * product.price
             product_count += item_data
+            # Append item details to bag_items list
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
             })
         else:
+            # Retrieve the product using item_id
             product = get_object_or_404(Product, pk=item_id)
+            # Iterate over variants and quantities in item_data
             for variant, quantity in item_data['items_by_variant'].items():
+                # Calculate the price and quantity for the variant
                 total += quantity * product.price
                 product_count += quantity
+                # Append variant item details to bag_items list
                 bag_items.append({
                     'item_id': item_id,
                     'quantity': quantity,
@@ -33,15 +52,21 @@ def bag_contents(request):
                     'variant': variant,
                 })
 
+    # Check if the total price is below the free delivery threshold
     if total < settings.FREE_DELIVERY_THRESHOLD:
+        # Calculate the delivery cost as a percentage of the total price
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        # Calculate the remaining amount for free delivery
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
+        # No delivery cost if total price exceeds the free delivery threshold
         delivery = 0
         free_delivery_delta = 0
 
+    # Calculate the grand total including delivery cost
     grand_total = delivery + total
 
+    # Create a context dictionary with all the calculated values
     context = {
         'bag_items': bag_items,
         'total': total,
@@ -52,4 +77,5 @@ def bag_contents(request):
         'grand_total': grand_total,
     }
 
+    # Return the context dictionary
     return context
--- bag/views.py
+++ bag/views.py
@@ -4,8 +4,6 @@ from django.contrib import messages
 
 from products.models import Product
 
-# Create your views here.
-
 
 def view_bag(request):
     """ A view to return the shopping bag page """
@@ -14,45 +12,72 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """
+    Add a quantity of the specified product to the shopping bag
+    """
 
+    # Get the product object with the specified item_id from the database
     product = Product.objects.get(pk=item_id)
+
+    # Extract the quantity of the product from the POST data of the request
     quantity = int(request.POST.get('quantity'))
+
+    # Extract the redirect URL from the POST data of the request
     redirect_url = request.POST.get('redirect_url')
+
+    # Initialize a variable to store the product variant (if any)
     variant = None
+
+    # Check if the 'product_variant' key is present in the POST data
     if 'product_variant' in request.POST:
         variant = request.POST['product_variant']
+
+    # Get the current bag from the session or
+    # initialize an empty bag if it doesn't exist
     bag = request.session.get('bag', {})
 
+    # Check if the product has a variant
     if variant:
+        # Check if the item_id already exists in the bag
         if item_id in list(bag.keys()):
+            # Check if the variant already exists for the item_id in the bag
             if variant in bag[item_id]['items_by_variant'].keys():
+                # Update the quantity of the existing variant in the bag
                 message_e = (f'Updated  {product.name} for {variant.upper()} '
                              f'quantity to '
                              f'{bag[item_id]["items_by_variant"][variant]}')
                 bag[item_id]['items_by_variant'][variant] += quantity
                 messages.success(request, message_e)
             else:
+                # Add a new variant to the item_id in the bag
                 message_a = (f'Added {product.name} for {variant.upper()} '
                              'to your bag')
                 bag[item_id]['items_by_variant'][variant] = quantity
                 messages.success(request, message_a)
         else:
+            # Add a new item_id with a variant to the bag
             message_b = (f'Added {product.name} for {variant.upper()} '
                          'to your bag')
             bag[item_id] = {'items_by_variant': {variant: quantity}}
             messages.success(request, message_b)
     else:
+        # The product does not have a variant
+        # Check if the item_id already exists in the bag
         if item_id in list(bag.keys()):
+            # Update the quantity of the existing item_id in the bag
             message_c = f'Updated {product.name} quantity to {bag[item_id]}'
             bag[item_id] += quantity
             messages.success(request, message_c)
         else:
+            # Add a new item_id without a variant to the bag
             message_d = f'Added {product.name} to your bag'
             bag[item_id] = quantity
             messages.success(request, message_d)
 
+    # Update the bag in the session with the modified or new items
     request.session['bag'] = bag
+
+    # Redirect to the specified URL
     return redirect(redirect_url)
 
 
@@ -60,35 +85,60 @@ def adjust_bag(request, item_id):
     """ Add or remove a quantity of the specified product
     to the shopping bag """
 
+    # Get the product object with the specified item_id from the database or
+    # return a 404 error if not found
     product = get_object_or_404(Product, pk=item_id)
+
+    # Extract the quantity of the product from the POST data of the request
     quantity = int(request.POST.get('quantity'))
+
+    # Initialize a variable to store the product variant (if any)
     variant = None
+
+    # Check if the 'product_variant' key is present in the POST data
     if 'product_variant' in request.POST:
         variant = request.POST['product_variant']
+
+    # Get the current bag from the session or initialize
+    # an empty bag if it doesn't exist
     bag = request.session.get('bag', {})
 
+    # Check if the product has a variant
     if variant:
+        # Check if the quantity is greater than 0
         if quantity > 0:
+            # Update the quantity of the variant in the bag
             bag[item_id]['items_by_variant'][variant] = quantity
-            messages.success(request, f'Updated {product.name} for '
-                             f'{variant.upper()} quantity to '
-                             f'{bag[item_id]["items_by_variant"][variant]}')
+            messages.success(
+                request, f'Updated {product.name} for {variant.upper()} '
+                f'quantity to {bag[item_id]["items_by_variant"][variant]}'
+                )
         else:
+            # Remove the variant from the item_id in the bag
             del bag[item_id]['items_by_variant'][variant]
             if not bag[item_id]['items_by_variant']:
-                bag.pop[item_id]
+                # If there are no more variants for the item_id,
+                # remove the item_id from the bag
+                bag.pop(item_id)
             messages.success(request, f'Removed {product.name} for '
-                                      f'{variant.upper()} from your bag')
+                             f'{variant.upper()} from your bag')
     else:
+        # The product does not have a variant
+        # Check if the quantity is greater than 0
         if quantity > 0:
+            # Update the quantity of the item_id in the bag
             bag[item_id] = quantity
             messages.success(request, f'Updated {product.name} '
                              f'quantity to {bag[item_id]}')
         else:
+            # Remove the item_id from the bag
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')
 
+    # Update the bag in the session with the modified or removed items
     request.session['bag'] = bag
+
+    # Redirect to the 'view_bag' page
     return redirect(reverse("view_bag"))
 
 
@@ -97,25 +147,48 @@ def remove_from_bag(request, item_id):
     from the shopping bag """
 
     try:
+        # Get the product object with the specified item_id
+        # from the database or return a 404 error if not found
         product = get_object_or_404(Product, pk=item_id)
+
+        # Initialize a variable to store the product variant (if any)
         variant = None
+
+        # Check if the 'product_variant' key is present in the POST data
         if 'product_variant' in request.POST:
             variant = request.POST['product_variant']
+
+        # Get the current bag from the session or initialize an
+        # empty bag if it doesn't exist
         bag = request.session.get('bag', {})
 
+        # Check if the product has a variant
         if variant:
+            # Remove the variant from the item_id in the bag
             del bag[item_id]['items_by_variant'][variant]
+
+            # If there are no more variants for the item_id, remove
+            # the item_id from the bag
             if not bag[item_id]['items_by_variant']:
                 bag.pop(item_id)
-            message_a = (f'Removed variant {variant.upper()} '
-                         f'{product.name} from your bag')
+
+            message_a = (
+                f'Removed variant {variant.upper()}'
+                f'{product.name} from your bag'
+                )
             messages.success(request, message_a)
         else:
+            # Remove the item_id from the bag
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')
 
+        # Update the bag in the session with the removed item or variant
         request.session['bag'] = bag
+
+        # Return an HTTP response with a success status code (200)
         return HttpResponse(status=200)
     except Exception as e:
+        # If an exception occurs, display an error message
+        # return an HTTP response with an error status code (500)
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
