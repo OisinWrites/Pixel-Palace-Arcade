ef6414e2a727b44e176d9d21d18ee7a559fbd234
OisinWrites <oisin.banville@gmail.com>
2023-07-12 16:04:53 +0000

    Add def for snake movement, and for the game to keep track of its location. Add wall collision detection

--- snake/models.py
+++ snake/models.py
@@ -38,20 +38,31 @@ class Game(models.Model):
             ]
         self.snake_body = self._serialize_body(snake_body)
 
+    """Retrieves the snake's body as a list of tuples representing
+       each segment's position."""
     def get_snake_body(self):
         return self._deserialize_body(self.snake_body)
 
+    """Updates the snake's body position with a new body provided
+       as a list of tuples."""
     def update_snake_position(self, new_body):
         self.snake_body = self._serialize_body(new_body)
 
+    """Serializes the snake's body from a list of tuples into a
+       string representation."""
     def _serialize_body(self, body):
         return ','.join(f'{row},{col}' for row, col in body)
 
+    """Deserializes the snake's body from a string representation
+       into a list of tuples."""
     def _deserialize_body(self, body_str):
-        return [tuple(map(int, segment.split(','))) for segment in body_str.split(',')]
+        return [tuple(map(int, segment.split(','))
+                      ) for segment in body_str.split(',')]
 
+    """Check for snake hitting walls"""
     def check_collision(self, next_row, next_col):
-        if next_row < 0 or next_row >= self.board_height or next_col < 0 or next_col >= self.board_width:
+        if next_row < 0 or next_row >= self.board_height or \
+           next_col < 0 or next_col >= self.board_width:
             return True
 
         snake_body = self.get_snake_body()
@@ -59,6 +70,28 @@ class Game(models.Model):
             return True
 
         return False
+
+        """Set control meanings for snake direction input"""
+    def move_snake(self):
+        snake_head = self.get_snake_head()
+        snake_body = self.get_snake_body()
+
+        if not snake_head:
+            return
+
+        next_row, next_col = snake_head
+        if self.direction == 'up':
+            next_row -= 1
+        elif self.direction == 'down':
+            next_row += 1
+        elif self.direction == 'left':
+            next_col -= 1
+        elif self.direction == 'right':
+            next_col += 1
+        
+        new_body = [snake_head] + snake_body[:-1]
+        self.update_snake_position(new_body)
+
 
 class Score(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
