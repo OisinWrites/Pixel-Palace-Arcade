e5b7694ee21f64736dfb54d5f35482b362a0a8df
OisinWrites <oisin.banville@gmail.com>
2023-07-12 15:50:02 +0000

    Create snake intialise method, and score model

--- snake/models.py
+++ snake/models.py
@@ -7,3 +7,41 @@ class Player(models.Model):
     avatar_name = models.CharField(max_length=255)
     games_played = models.PositiveIntegerField(default=0)
     highscore = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.avatar_name:
+            self.avatar_name = self.user.username
+        super().save(*args, **kwargs)
+
+    def increment_games_played(self):
+        self.games_played += 1
+
+    def update_highscore(self, score):
+        if score > self.highscore:
+            self.highscore = score
+
+
+class Game(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+    board_width = models.PositiveIntegerField()
+    board_height = models.PositiveIntegerField()
+    snake_body = models.TextField(default='')
+    food_position_row = models.PositiveIntegerField()
+    food_position_col = models.PositiveIntegerField()
+
+    def initialize_snake(self, initial_length):
+        initial_row = self.board_height // 2
+        initial_col = self.board_width // 2
+        snake_body = [
+            (initial_row, initial_col - i) for i in range(initial_length)
+            ]
+        self.snake_body = self._serialize_body(snake_body)
+
+
+class Score(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    score = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.player.avatar_name}: {self.score}'
--- snake/urls.py
+++ snake/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('game/', views.game, name='game'),
-    path('leaderboard/', views.leaderboard, name='leaderboard'),
+
 ]
+
+"""path('', views.home, name='home'),
+path('game/', views.game, name='game'),
+path('leaderboard/', views.leaderboard, name='leaderboard'),"""
