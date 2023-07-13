a955ff69f60df8703d92387c3870318055b77a21
OisinWrites <oisin.banville@gmail.com>
2023-07-13 17:23:51 +0000

    Create view for snake game

--- snake/views.py
+++ snake/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Player, Game, Score
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def game(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    player = Player.objects.get(user=request.user)
+    game = Game.objects.create(player=player, board_width=20, board_height=20)
+    game.initialize_snake(initial_length=4)
+    game.generate_food()
+
+    context = {
+        'player': player,
+        'game': game,
+    }
+    return render(request, 'game.html', context)
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def leaderboard(request):
+    scores = Score.objects.order_by('-score')[:10]
+    context = {
+        'scores': scores,
+    }
+    return render(request, 'leaderboard.html', context)
