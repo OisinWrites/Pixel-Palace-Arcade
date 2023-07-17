06b8963eaae69f0597282bcf5a7006e2de5f954e
OisinWrites <oisin.banville@gmail.com>
2023-07-17 21:15:50 +0000

    Style the checkout page and correct the automatil population of the user name field in the details form.

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,11 +3,11 @@ from . import views
 from .webhooks import webhook
 
 urlpatterns = [
-    path('', views.checkout, name='checkout'),
-    path('checkout_success/<order_number>', views.checkout_success,
-         name='checkout_success'),
-    path('cache_checkout_data/', views.cache_checkout_data,
-         name='cache_checkout_data'),
-    path('wh/', webhook, name='webhook'),
-    path('admin/orders/', views.admin_order_list, name='admin_order_list'),
-]
+     path('', views.checkout, name='checkout'),
+     path('checkout_success/<order_number>', views.checkout_success,
+          name='checkout_success'),
+     path('cache_checkout_data/', views.cache_checkout_data,
+          name='cache_checkout_data'),
+     path('wh/', webhook, name='webhook'),
+     path('admin/orders/', views.admin_order_list, name='admin_order_list'),
+     ]
