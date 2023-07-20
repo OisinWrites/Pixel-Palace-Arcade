c385c4b5595b521b9304dddc9036345209024297
OisinWrites <oisin.banville@gmail.com>
2023-07-20 09:18:54 +0000

    Edit return page button, edit the product edit form and custom image widget

--- products/forms.py
+++ products/forms.py
@@ -12,16 +12,37 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['price', 'image_url', 'image', 'name',
+                  'category', 'description', 'has_variants']
 
     image = forms.ImageField(label='Image', required=False,
-                             widget=CustomClearableFileInput)
+                             widget=CustomClearableFileInput,)
+
+    price = forms.DecimalField(label='Price', max_digits=6, decimal_places=2,
+                               widget=forms.NumberInput(
+                                attrs={'placeholder': 'Enter price...'}))
+
+    image_url = forms.URLField(label='Image URL', max_length=1824,
+                               widget=forms.TextInput(
+                                attrs={'placeholder': 'Enter image URL...'}))
+
+    name = forms.CharField(label='Product Name', max_length=254,
+                           widget=forms.TextInput(
+                            attrs={'placeholder': 'Enter product name...'}))
+
+    category = forms.ModelChoiceField(
+        label='Category', queryset=Category.objects.all(),
+        widget=forms.Select(attrs={'class': 'rounded-1'}))
+
+    description = forms.CharField(
+        label='Description', widget=forms.Textarea(
+            attrs={'placeholder': 'Enter product description...'}))
+
+    has_variants = forms.BooleanField(
+        label='Has Variants', required=False,
+        widget=forms.CheckboxInput(attrs={'class': 'rounded-1'}))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
-        self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs['class'] = 'rounded-1'
