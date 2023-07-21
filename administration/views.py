722720e6330a74afdd009f78cee08bc3de2671f5
OisinWrites <oisin.banville@gmail.com>
2023-07-21 03:31:22 +0000

    Remove cancellation email function, no time to implement properly

--- administration/views.py
+++ administration/views.py
@@ -93,29 +93,6 @@ def pending_orders(request):
 def completed_orders(request):
     complete_orders = Order.objects.filter(completed=True)
 
-    def _send_shipping_cancellation_email(order):
-        """Send the user a shipping confirmation email"""
-        cust_email = order.email
-        subject = render_to_string(
-            'checkout/confirmation_emails/shipping_cancel_subject.txt',
-            {'order': order}
-        )
-        body = render_to_string(
-            'checkout/confirmation_emails/shipping_cancel_body.txt',
-            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL}
-        )
-
-        # Print the email content to the terminal
-        print("Subject: ", subject)
-        print("Body: ", body)
-
-        send_mail(
-            subject,
-            body,
-            settings.DEFAULT_FROM_EMAIL,
-            [cust_email]
-        )
-
     if request.method == 'POST':
         # Handle the cancel form submission
         cancel_form = MarkOrderIncompleteForm(request.POST)
@@ -125,9 +102,6 @@ def completed_orders(request):
                 order_to_cancel = Order.objects.get(id=order_id)
                 order_to_cancel.completed = False
                 order_to_cancel.save()
-
-                _send_shipping_cancellation_email(order)
-
                 # Redirect to the same page after canceling the order
                 return redirect('completed_orders')
             except Order.DoesNotExist:
